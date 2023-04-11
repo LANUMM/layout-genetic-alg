6966d1d4dd8f33c4581e325ba660dece5a9f67cc
LANUMM <matthewlanum@gmail.com>
2023-04-11 02:26:48 -0400

    started scoring

--- genetic_functions.py
+++ genetic_functions.py
@@ -1,4 +1,10 @@
 import numpy as np
+import pandas as pd
+
+from helper_functions import get_weighted_from_to
+
+# GLOBAL VARIABLES
+weighted_from_to = get_weighted_from_to()
 
 
 # Returns an array of pop_size number of totally randomized possible layouts
@@ -13,7 +19,8 @@ def generate_initial_population(pop_size: int):
 # Test: 5x5 array, 5 1s, 5 2s, 5 3s, 5 4s, 5 5s
 def generate_random_sample():
     # Create an empty 5x5 array
-    genome = np.zeros((5, 5), dtype=int)
+    genome = pd.DataFrame(columns=['y1', 'y2', 'y3', 'y4', 'y5'], index=['x1', 'x2', 'x3', 'x4', 'x5'])
+    genome = genome.fillna(0)
 
     machine_types = [1, 2, 3, 4, 5]
 
@@ -26,8 +33,38 @@ def generate_random_sample():
 
     # Fill 5x5 array with machines
     for i, coordinate in enumerate(machine_coordinates):
-        genome[coordinate] = machines[i]
+        genome.iloc[coordinate[0], coordinate[1]] = machines[i]
 
     return genome
 
 
+def score_pop_fitness(pop: list):
+    # List of all scored_pops
+    pop_scores = []
+    for genome in pop:
+        scored_genome = {
+            "genome": genome,
+            "score": score_genome_fitness(genome)
+        }
+        pop_scores.append(scored_genome)
+
+    return pop_scores
+
+
+def score_genome_fitness(genome):
+    score = 0
+    for row_idx in range(len(weighted_from_to.index)):
+        for col_idx in range(len(weighted_from_to.columns)):
+            if row_idx > 0:
+                if col_idx < 6:
+                    from_op = row_idx
+                    to_op = col_idx
+                    myVal = weighted_from_to.iloc[row_idx,col_idx]
+                    a=1
+    print(weighted_from_to)
+    print(genome)
+    return 9
+
+
+my_pop = generate_initial_population(5)
+scored_pop = score_pop_fitness(my_pop)
